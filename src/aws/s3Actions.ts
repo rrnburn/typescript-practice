6af6dca6bc25a089f6334d26ab6b86be2113d0c0
Robert Burnett <rburnett@caci.co.uk>
2026-02-16 10:58:20 +0000

    added s3 actions

---
+++ src/aws/s3Actions.ts
@@ -0,0 +1,71 @@
+import { S3Client, ListBucketsCommand, ListObjectsCommand, GetObjectCommand, PutObjectCommand, CreateBucketCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({ apiVersion: "2006-03-01" });
+
+export const s3Actions = {
+  async handleS3Get(payload: any): Promise<any> {
+    switch (payload.action) {
+      case 'listBuckets':
+        return await listBuckets();
+      case 'listObjects':
+        return await listObjects(payload.bucketName);
+      case 'getObject':
+        return await getObject(payload.bucketName, payload.objectKey);
+      default:
+        throw new Error(`Unknown S3 action: ${payload.action}`);
+    }
+  },
+  async handleS3Put(payload: any): Promise<any> {
+    switch (payload.action) {
+      case 'createBucket':
+        return await createBucket(payload);
+      case 'putObject':
+        return await putObject(payload);
+      default:
+        throw new Error(`Unknown S3 action: ${payload.action}`);
+    }
+  },
+  async handleS3Delete(payload: any): Promise<any> {
+    const { bucketName, objectKey } = payload;
+    const response = await s3.send(new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: objectKey
+    }));
+    return response;
+  },
+};
+
+async function createBucket(payload: any): Promise<any> {
+    const { bucketName } = payload;
+    const response = await s3.send(new CreateBucketCommand({
+      Bucket: bucketName
+    }));
+    return response;
+};
+
+async function putObject(payload: any): Promise<any> {
+    const { bucketName, objectKey, body } = payload;
+    const response = await s3.send(new PutObjectCommand({
+      Bucket: bucketName,
+      Key: objectKey,
+      Body: body
+    }));
+    return response;
+};
+
+async function listBuckets(): Promise<any> {
+    const command = new ListBucketsCommand({});
+    const response = await s3.send(command);
+    return response.Buckets || [];
+};
+async function listObjects(bucketName: string): Promise<any> {
+    const command = new ListObjectsCommand({ Bucket: bucketName });
+    const response = await s3.send(command);
+    return response.Contents || [];
+};
+
+async function getObject(bucketName: string, objectKey: string): Promise<any> {
+    const command = new GetObjectCommand({ Bucket: bucketName, Key: objectKey });
+    const response = await s3.send(command);
+    return response.Body;
+};
--- src/handlers/eventHandler.ts
+++ src/handlers/eventHandler.ts
@@ -1,39 +1,51 @@
 import { dynamoActions } from '../aws/dynamoActions';
+import { s3Actions } from '../aws/s3Actions';
 
 export interface Event {
+  type: string;
   action: string;
   payload: any;
 }
 
 export async function eventHandler(event: Event): Promise<any> {
-  const { action, payload } = event;
+  const { type, action, payload } = event;
 
+  switch (type) {
+    case 'dynamo':
+      return await handleDynamoEvent(action, payload);
+    case 's3':
+      return await handleS3Event(action, payload);
+    default:
+      throw new Error(`Unknown event type: ${type}`);
+  }
+}
+
+async function handleS3Event(action: string, payload: any): Promise<any> {
   switch (action) {
     case 'put':
-      return await handlePut(payload);
+      return await s3Actions.handleS3Put(payload);
     case 'get':
-      return await handleGet(payload);
-    case 'query':
-      return await handleQuery(payload);
+      return await s3Actions.handleS3Get(payload);
     case 'delete':
-      return await handleDelete(payload);
+      return await s3Actions.handleS3Delete(payload);
+    default:
+      throw new Error(`S3 event handling not implemented for action: ${action}`);
+  }
+};
+
+async function handleDynamoEvent(action: string, payload: any): Promise<any> {
+  switch (action) {
+    case 'put':
+      return await dynamoActions.putItem(payload.tableName, payload.item);
+    case 'get':
+      return await dynamoActions.getItem(payload.tableName, payload.key);
+    case 'query':
+      return await dynamoActions.queryItems(payload.tableName, payload.keyCondition);
+    case 'delete':
+      return await dynamoActions.deleteItem(payload.tableName, payload.key);
     default:
       throw new Error(`Unknown action: ${action}`);
   }
 }
 
-async function handlePut(payload: any): Promise<any> {
-  return await dynamoActions.putItem(payload.tableName, payload.item);
-}
 
-async function handleGet(payload: any): Promise<any> {
-  return await dynamoActions.getItem(payload.tableName, payload.key);
-}
-
-async function handleQuery(payload: any): Promise<any> {
-  return await dynamoActions.queryItems(payload.tableName, payload.keyCondition);
-}
-
-async function handleDelete(payload: any): Promise<any> {
-  return await dynamoActions.deleteItem(payload.tableName, payload.key);
-}
